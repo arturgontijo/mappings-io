64e787480e319815ca61015afbb604ba6c8718d2
Artur Gontijo <arturgontijo@gmail.com>
2024-05-02 14:18:24 -0300

    [Lint] Adding eslintPluginPrettierRecommended

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,11 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 ];
